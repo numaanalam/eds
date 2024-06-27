e42135bf1b0831cfd67914cfda72cde363811cb7
Mohd Numaan Alam <40804220+numaanalam@users.noreply.github.com>
2024-06-27 17:28:37 +0530

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -2,10 +2,10 @@ export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
+  blockquote.textContent = "Prasad";
   quoteWrapper.replaceChildren(blockquote);
 
   const blockquote1 = document.createElement('blockquote1');
-  blockquote1.textContent =quoteWrapper.textContent.trim();
+  blockquote1.textContent ="Shivam";
   quoteWrapper.replaceChildren(blockquote1);
 }
