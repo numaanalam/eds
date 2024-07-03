5aef089d9438ee9c2c3c634b5026e2ae6a2f07a8
Mohd Numaan Alam <40804220+numaanalam@users.noreply.github.com>
2024-07-03 15:30:17 +0530

    applied some changes

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,5 +1,56 @@
-export default function decorate(block){
-  const[background, foreground] = block.children;
-  background.className="bg";
-  foreground.className="fg";
+export default function decorateTeaser(block) {
+  function getTeaserData(block) {
+    const [backgroundContainer, foregroundContainer] = block.children;
+
+    const backgroundImageEl = backgroundContainer.querySelector('picture');
+    const backgroundImageAltEl = backgroundContainer.querySelector('img');
+
+    const preTitleEl = foregroundContainer.querySelector('p');
+    const titleEl = foregroundContainer.querySelector('h3');
+    const descriptionEl = foregroundContainer.querySelector('.description');
+
+    const backgroundImg = backgroundImageEl?.querySelector('img');
+    if (backgroundImg) {
+      backgroundImg.removeAttribute('width');
+      backgroundImg.removeAttribute('height');
+      const alt = backgroundImageAltEl?.getAttribute('alt') || 'image';
+      backgroundImg.setAttribute('alt', alt);
+      backgroundImg.style.width = '100%';
+      backgroundImg.style.height = '50%';
+      backgroundImg.style.objectFit = 'cover';
+      backgroundImg.style.opacity = '0.75';
+    }
+
+    const pretitle = preTitleEl?.textContent.trim();
+    const title = titleEl?.textContent.trim();
+    const description = Array.from(descriptionEl ? [descriptionEl] : []).map((p) => p.outerHTML).join('');
+
+    return {
+      backgroundImg,
+      pretitle,
+      title,
+      description
+    };
+  }
+
+  const teaserData = getTeaserData(block);
+  const teaserHtml = `
+    <div class="teaser_wrapper">
+      ${teaserData.backgroundImg ? teaserData.backgroundImg.outerHTML : ''}
+      <div class="teaser_content">
+        ${teaserData.pretitle ? `<p>${teaserData.pretitle}</p>` : ''}
+        ${teaserData.title ? `<h3>${teaserData.title}</h3>` : ''}
+        ${teaserData.description ? `${teaserData.description}` : ''}
+      </div>
+    </div>
+  `;
+
+  block.innerHTML = teaserHtml;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const teaserBlock = document.querySelector('.block.teaser');
+  if (teaserBlock) {
+    decorateTeaser(teaserBlock);
+  }
+});
