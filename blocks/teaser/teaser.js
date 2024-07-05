6c3928facbf7d1f343285e9ba91b5811efc71cb2
Mohd Numaan Alam <40804220+numaanalam@users.noreply.github.com>
2024-07-05 15:01:09 +0530

    Update teaser.js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -32,61 +32,46 @@ export default function decorate(block) {
 
 
 
+  // //using utility.sanitize method
+  //   const teaserHtml = utility.sanitizeHtml(` 
+  //       ${teaserData.pretitle ? `<p>${teaserData.pretitle}</p>` : ''}
+  //         ${teaserData.backgroundImg ? teaserData.backgroundImg.outerHTML : ''}
+  //         <div class="teaser_content">
+  //           ${teaserData.title ? `<h3>${teaserData.title}</h3>` : ''}
+  //           ${teaserData.description ? `${teaserData.description}` : ''}
+  //         </div>
+  //     `);
 
-  const teaserHtml = utility.sanitizeHtml(` 
-      ${teaserData.pretitle ? `<p>${teaserData.pretitle}</p>` : ''}
-        ${teaserData.backgroundImg ? teaserData.backgroundImg.outerHTML : ''}
-        <div class="teaser_content">
-          ${teaserData.title ? `<h3>${teaserData.title}</h3>` : ''}
-          ${teaserData.description ? `${teaserData.description}` : ''}
-        </div>
-    `);
+  //   // block.innerHTML = teaserHtml;
+
+  //   block.innerHTML = `
+  //       <div class= "teaser_wrapper">
+  //         ${teaserHtml}
+  //       </div>
+  //    `;
+
+
+
+
+
+  const teaserHtml = ` 
+  ${teaserData.pretitle ? `<p>${teaserData.pretitle}</p>` : ''}
+    ${teaserData.backgroundImg ? teaserData.backgroundImg.outerHTML : ''}
+    <div class="teaser_content">
+      ${teaserData.title ? `<h3>${teaserData.title}</h3>` : ''}
+      ${teaserData.description ? `${teaserData.description}` : ''}
+    </div>
+`;
 
   // block.innerHTML = teaserHtml;
 
   block.innerHTML = `
-      <div class= "teaser_wrapper">
-        ${teaserHtml}
-      </div>
-   `;
+  <div class= "teaser_wrapper">
+    ${teaserHtml}
+  </div>
+`;
 
 
-  //   const teaserHtml = utility.sanitizeHtml(`
-  //   ${teaserData.pretitle ? `<p class="teaser_pretitle">${teaserData.pretitle}</p>` : ''}
-  //   ${teaserData.backgroundImg ? `<div class="teaser_background">${teaserData.backgroundImg.outerHTML}</div>` : ''}
-  //   <div class="teaser_content">
-  //     ${teaserData.title ? `<h3 class="teaser_title">${teaserData.title}</h3>` : ''}
-  //     ${teaserData.description ? `<div class="teaser_description">${teaserData.description}</div>` : ''}
-  //   </div>
-  // `);
-
-  //   block.outerHTML = `
-  //   <div class="teaser_wrapper">
-  //     ${teaserHtml}
-  //   </div>
-  // `;
-
-  // const teaserHtml = utility.sanitizeHtml(`
-  //   <div class="teaser-background">
-  //     ${backgroundImageEl ? backgroundImageEl.outerHTML : ''}
-  //   </div>
-  //   <div class="pretitle">
-  //     ${teaserData.pretitle ? `<p">${teaserData.pretitle}</p>` : ''}
-  //   </div>
-  //   <div class="title">
-  //     ${teaserData.title ? `<p">${teaserData.title}</p>` : ''}
-  //   </div>
-  //   <div class="description">
-  //     ${description ? `<p class="teaser-description">${descriptionEl.textContent}</p>` : ''}
-  //   </div>
-  // `);
-
-  // // Set the innerHTML of the block to the constructed teaser HTML
-  // block.innerHTML = `
-  //   <div class="teaser-wrapper">
-  //     ${teaserHtml}
-  //   </div>
-  // `;
 
 
 }
