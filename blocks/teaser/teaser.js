1adfd885615587f537df60e7455e4886554e6d7b
Mohd Numaan Alam <40804220+numaanalam@users.noreply.github.com>
2024-07-05 16:21:14 +0530

    Update teaser.js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,5 +1,3 @@
-import utility from "../../utility/utility";
-
 export default function decorate(block) {
   function getTeaserData(block) {
     const [
@@ -42,25 +40,8 @@ export default function decorate(block) {
 
     </div>
   `;
+
   block.innerHTML = teaserHtml;
-
-
-  // //using utility.sanitize method.
-
-  // const teaserHtml = utility.sanitizeHtml(`
-  // <div class="teaser_wrapper">
-  // ${teaserData.pretitle ? `<p>${teaserData.pretitle}</p>` : ''}
-  //   ${teaserData.backgroundImg ? teaserData.backgroundImg.outerHTML : ''}
-  //   <div class="teaser_content">
-  //     ${teaserData.title ? `<h3>${teaserData.title}</h3>` : ''}
-  //     ${teaserData.description ? `${teaserData.description}` : ''}
-  //   </div>
-
-  // </div>
-  // `);
-  // block.innerHTML = teaserHtml;
-
-
 }
 
 
