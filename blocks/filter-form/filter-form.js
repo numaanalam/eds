7818fe21802ac612391e3e799611b5c8cc174b60
Mohd Numaan Alam <40804220+numaanalam@users.noreply.github.com>
2024-07-19 00:37:54 +0530

    Update filter-form.js

--- blocks/filter-form/filter-form.js
+++ blocks/filter-form/filter-form.js
@@ -1,54 +1,34 @@
-import utility from '../../utility/utility.js';
+
 export default function decorate(block) {
-  function getFormData(block) {
-    const [
-      placeholder1El,
-      placeholder2El,
-      placeholder3El,
-      button1El,
-      button2El
-    ] = block.children;
 
 
-
-    const placeholder1 = placeholder1El?.textContent.trim();
-    const placeholder2 = placeholder2El?.textContent.trim();
-    const placeholder3 = placeholder3El?.textContent.trim();
-    const button1 = button1El?.textContent.trim();
-    const button2 = button2El?.textContent.trim();
-
-
-
-    return {
-      placeholder1,
-      placeholder2,
-      placeholder3,
-      button1,
-      button2
-    };
-  }
-
-
-  const formData = getFormData(block);
-
-  const formHtml = utility.sanitizeHtml(`
-  <div class="teaser_wrapper">
-  ${formData.pretitle ? `<p>kdjkdkfj;fd</p>` : ''}
-   
+  const formHtml = `
+  <div class="New">
+   <p>New Finance Journey</p>  
   </div>
-`);
-
-  block.innerHTML = teaserHtml;
-
+`;
+  block.innerHTML = formHtml;
 
 
+  const placeholder1 = document.createElement("input");
+  placeholder1.placeholder = "Mobile no.";
+  block.appendChild(placeholder1);
 
+  const placeholder2 = document.createElement("input");
+  placeholder2.placeholder = "Booking ID";
+  block.appendChild(placeholder2);
 
+  const placeholder3 = document.createElement("input");
+  placeholder3.placeholder = "DMS Enquiry";
+  block.appendChild(placeholder3);
 
+  const button1 = document.createElement("button");
+  button1.textContent = "Search";
+  block.appendChild(button1);
 
-
-
-
+  const button2 = document.createElement("button");
+  button2.textContent = "Clear";
+  block.appendChild(button2);
 
 }
 
