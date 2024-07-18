1c0a961734d44b4df716d18a2c8b5b30d645d341
Mohd Numaan Alam <40804220+numaanalam@users.noreply.github.com>
2024-07-18 17:15:10 +0530

    a

---
+++ blocks/filter-form/filter-form.js
@@ -0,0 +1,77 @@
+import utility from '../../utility/utility.js';
+export default function decorate(block) {
+  function getformData(block) {
+    const [
+      placeholder1El,
+      placeholder2El,
+      placeholder3El,
+      button1El,
+      button2El
+    ] = block.children;
+
+
+
+    const placeholder1 = placeholder1El?.textContent.trim();
+    const placeholder2 = placeholder2El?.textContent.trim();
+    const placeholder3 = placeholder3El?.textContent.trim();
+    const button1 = button1El?.textContent.trim();
+    const button2 = button2El?.textContent.trim();
+
+
+
+    return {
+      placeholder1,
+      placeholder2,
+      placeholder3,
+      button1,
+      button2
+    };
+  }
+
+  const teaserData = getFormData(block);
+
+
+  // const teaserHtml = `
+  //   <div class="teaser_wrapper">
+  //   ${teaserData.pretitle ? `<p>${teaserData.pretitle}</p>` : ''}
+  //     ${teaserData.backgroundImg ? teaserData.backgroundImg.outerHTML : ''}
+  //     <div class="teaser_content">
+  //       ${teaserData.title ? `<h3>${teaserData.title}</h3>` : ''}
+  //       ${teaserData.description ? `${teaserData.description}` : ''}
+  //     </div>
+
+  //   </div>
+  // `;
+
+  // block.innerHTML = teaserHtml;
+
+
+  //using utility.sanitize()
+
+  const formHtml = utility.sanitizeHtml(`
+  <div class="form-container">
+  <form id="exampleForm">
+     
+      <input type="text" id="input1" name="input1" placeholder="${FormData.placeholder1}">
+
+      
+      <input type="text" id="input2" name="input2" placeholder="Enter second value">
+
+    
+      <input type="text" id="input3" name="input3" placeholder="Enter third value">
+
+      <button type="button" class="search-button" onclick="handleSearch()">Search</button>
+      <button type="reset" class="clear-button">Clear</button>
+  </form>
+</div>
+
+  `);
+
+  block.innerHTML = formHtml;
+
+
+
+}
+
+
+
